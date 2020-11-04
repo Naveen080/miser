0fe39e3af9cc4f3ff37137b51076c39bc0430c8e
Naveen080 <37764648+Naveen080@users.noreply.github.com>
2020-11-04 18:28:24 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/miser/demo/DemoApplicationTests.java
+++ src/test/java/com/miser/demo/DemoApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public String contextLoads() {
+		return "Hello Docker World";
 	}
 
 }
